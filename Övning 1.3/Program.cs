c33b57a8f0a5747d3c24259d7f36bed5564182b3
DiboCrafter <emsv06002@utb.vaxjo.se>
2024-11-13 12:24:37 +0100

    Add project files.

---
+++ Övning 1.3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Övning1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("  /\\  ");
+            Console.WriteLine(" /  \\ ");
+            Console.WriteLine("/____\\");
+        }
+    }
+}
